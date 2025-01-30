fd02ffaa43bbb6696cc32d08a83ddcfd081bab7f
dmurchison <duncancmurchison@gmail.com>
2025-01-30 15:28:18 -0500

    more nothingness

--- algorithms/warmup.py
+++ algorithms/warmup.py
@@ -112,3 +112,20 @@ def groupAnagrams(strs):
     return list(anagrams.values())
 
 print(groupAnagrams(["act","pots","tops","cat","stop","hat"]))
+
+
+def dutchFlagProb(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+    return arr
